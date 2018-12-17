5de2d7814960c924a011e3ae1183c4cbd1deffd2
doasync <doasync@gmail.com>
2018-12-17 17:05:49 +0300

    Refactoring, Flow types

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,5 +49,6 @@ module.exports = {
       'WithStatement',
       "BinaryExpression[operator='in']",
     ],
+    'spaced-comment': ['error', 'always', { markers: [':', '::'] }],
   },
 };
--- index.js
+++ index.js
@@ -1,75 +1,78 @@
+// @flow
 
 const React = require('react');
 const PropTypes = require('prop-types');
-
-const { useContext, useMemo } = React;
 const invariant = require('invariant');
 
-const componentModelMap = new WeakMap();
-const componentContextMap = new WeakMap();
+const { useContext, useMemo } = React;
 
-function getDisplayName (Component) {
+/*::
+type ModelData = {}
+type ModelOptions = {}
+type ModelFactory = (options: ?ModelOptions) => ModelData
+type ContextValue = {}
+type ComponentData = {
+  ComponentContext: React.Context<?ContextValue>,
+  modelFactory: ModelFactory
+}
+type ComponentType = React.ComponentType<{}>
+type BoundProvider = React.ComponentType<{
+  children: React.Node,
+  disable?: boolean,
+  options?: ModelOptions
+}>
+type BoundComponent = ComponentType | BoundProvider
+// $FlowIgnore
+type DataMap = WeakMap<BoundComponent, ComponentData>
+type ConsumerProps = {
+  children: (value: ?ContextValue) => ?React.Node,
+  of: BoundComponent,
+}
+*/
+
+const componentDataMap /*: DataMap */ = new WeakMap();
+
+function getDisplayName (Component /*: BoundComponent */) /*: string */ {
+  // flowlint-next-line sketchy-null-string: off
   return Component.displayName || Component.name || 'Component';
 }
 
-function bindModel (
-  Component,
-  componentModel,
-  Context = React.createContext(),
-) {
-  componentModelMap.set(Component, componentModel);
-  componentContextMap.set(Component, Context);
+function bindData (
+  Component /*: ComponentType */,
+  data /*: ComponentData */,
+) /*: void */ {
+  componentDataMap.set(Component, data);
 }
 
-function getBoundData (Component) {
-  const modelFactory = componentModelMap.get(Component);
-  const ComponentContext = componentContextMap.get(Component);
+function getBoundData (Component /*: BoundComponent */) /*: ComponentData */ {
+  const componentData = componentDataMap.get(Component);
 
-  invariant(
-    modelFactory && ComponentContext,
-    `You need to bind a model to your component: ${getDisplayName(Component)}`,
-  );
+  invariant(componentData, `You need to bind a model to your component "${getDisplayName(Component)}" using bindModel`);
 
-  return { modelFactory, ComponentContext };
+  return componentData;
 }
 
-function useComponentModel (Component, Provider) {
-  invariant(
-    typeof Component === 'function',
-    'You need to pass a component to useModel',
-  );
-
-  const { modelFactory, ComponentContext } = getBoundData(Component);
-  const { ComponentContext: ProviderContext } = Provider
-    ? getBoundData(Provider)
-    : {};
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  return (
-    useContext(ProviderContext || ComponentContext) || useMemo(modelFactory)
-  );
-}
-
-function createComponentProvider (Component, options) {
-  invariant(
-    typeof Component === 'function',
-    'You need to pass a component to createComponentProvider',
-  );
-
-  const { modelFactory } = getBoundData(Component);
-  const Context = React.createContext();
-
-  const Provider = ({ children, disable, options: optionsFromProp }) => {
+function providerFactory (
+  Context /*: React.Context<?ContextValue> */,
+  modelFactory /*: ModelFactory */,
+  options /*: ?ModelOptions */,
+) /*: BoundProvider */ {
+  const Provider = ({
+    children,
+    disable,
+    options: optionsFromProp,
+  }) => {
     const contextValue = disable
       ? undefined
       : modelFactory(optionsFromProp || options);
 
-    return React.createElement(
-      Context.Provider,
-      { value: contextValue },
-      children,
-    );
+    return React.createElement(Context.Provider, { value: contextValue }, children);
   };
+
+  bindData(Provider, {
+    modelFactory,
+    ComponentContext: Context,
+  });
 
   Provider.propTypes = {
     disable: PropTypes.bool,
@@ -82,17 +85,75 @@ function createComponentProvider (Component, options) {
     options: undefined,
   };
 
-  bindModel(Provider, modelFactory, Context);
-
   return Provider;
 }
 
-function Consumer (props) {
+function bindModel (
+  Component /*: ComponentType */,
+  modelFactory /*: ModelFactory */,
+  ComponentContext /*: React.Context<?ContextValue> */ = React.createContext(),
+) /*: void */ {
+  invariant(
+    typeof Component === 'function'
+    && typeof modelFactory === 'function'
+    && ComponentContext
+    && ComponentContext.Provider
+    && ComponentContext.Consumer,
+    'bindModel expects a component and a model factory (optionally a context)',
+  );
+
+  bindData(Component, {
+    modelFactory,
+    ComponentContext,
+  });
+
+  // eslint-disable-next-line no-param-reassign
+  Component.Consumer = ComponentContext.Consumer;
+  // eslint-disable-next-line no-param-reassign
+  Component.Provider = providerFactory(ComponentContext, modelFactory);
+}
+
+function useComponentModel (
+  Component /*: BoundComponent */,
+  Provider /*: ?BoundProvider */,
+) /*: ModelData */ {
+  invariant(
+    typeof Component === 'function',
+    'useComponentModel expects a component',
+  );
+
+  const { modelFactory, ComponentContext } = getBoundData(Component);
+  const { ComponentContext: ProviderContext } = Provider
+    ? getBoundData(Provider)
+    : {};
+
+  return (
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useContext(ProviderContext || ComponentContext) || useMemo(modelFactory)
+  );
+}
+
+function createComponentProvider (
+  Component /*: BoundComponent */,
+  options /*: ?ModelOptions */,
+) /*: BoundProvider */ {
+  invariant(
+    typeof Component === 'function',
+    'createComponentProvider expects a component',
+  );
+
+  const { modelFactory } = getBoundData(Component);
+  const Context = React.createContext();
+
+  return providerFactory(Context, modelFactory, options);
+}
+
+function Consumer (props /*: ConsumerProps */) {
   const { children, of: Component } = props;
 
   invariant(
     typeof children === 'function',
-    'Consumer expects a single child that is a function must be a function',
+    'Consumer expects a single child that is a function',
   );
 
   const { ComponentContext } = getBoundData(Component);
